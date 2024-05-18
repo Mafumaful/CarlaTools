f83b18e3bba40a06fee17d89a3d31a278a4f54f9
mafumaful <s6bian@uwaterloo.ca>
2024-05-17 21:53:10 -0400

    chore: Remove unnecessary code for drawing spawn point number

---
+++ move_spawnpt.py
@@ -0,0 +1,51 @@
+import carla
+import argparse
+
+argparser = argparse.ArgumentParser(description="Move the spectator to a specific spawn point")
+# add index argument
+argparser.add_argument(
+    '-i', '--index',
+    metavar='N',
+    default=0,
+    type=int,
+    help='Index of the spawn point to move the spectator to')
+
+def move_spectator_to_spawn_point(world, spawn_point_index):
+    # Get the map
+    carla_map = world.get_map()
+    
+    # Retrieve all the spawn points
+    spawn_points = carla_map.get_spawn_points()
+    
+    if spawn_points and 0 <= spawn_point_index < len(spawn_points):
+        spawn_point = spawn_points[spawn_point_index]
+        
+        # Get the spectator object
+        spectator = world.get_spectator()
+        
+        # Move the spectator to the specified spawn point
+        spectator.set_transform(carla.Transform(spawn_point.location + carla.Location(z=1.5),
+                                                spawn_point.rotation))
+        print(f"Moved spectator to spawn point #{spawn_point_index}")
+    else:
+        print(f"Invalid spawn point index: {spawn_point_index}")
+
+def main():
+    # Connect to the CARLA server
+    client = carla.Client('localhost', 2000)
+    client.set_timeout(10.0)
+    
+    # Get the world object
+    world = client.get_world()
+    
+    # Specify the spawn point index to move to
+    spawn_point_index = argparser.parse_args().index
+    
+    # Move the spectator to the specified spawn point
+    move_spectator_to_spawn_point(world, spawn_point_index)
+
+if __name__ == '__main__':
+    try:
+        main()
+    except KeyboardInterrupt:
+        print('Cancelled by user. Bye!')
--- visualize_sp.py
+++ visualize_sp.py
@@ -34,7 +34,6 @@ def main():
                                arrow_size=0.2,
                                color=carla.Color(0, 255, 0, 0),  # green color with no alpha transparency
                                life_time=10.0)
-        
         # Draw the spawn point number
         world.debug.draw_string(spawn_point.location + carla.Location(z=2),
                                 f'#{i}',
